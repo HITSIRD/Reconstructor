08c2b84b813d4d054f29ebe30fa92e1a72a80298
wenyongyan <wenyongyan@icloud.com>
2021-10-14 19:20:18 +0800

    v0.0

---
+++ cmake-build-release/CMakeFiles/Reconstructor.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/Camera.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/Camera.cpp.o"
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/Correction.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/Correction.cpp.o"
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/Image.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/Image.cpp.o"
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/Model.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/Model.cpp.o"
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/SfIS.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/SfIS.cpp.o"
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/iodata.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/iodata.cpp.o"
+  "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/main.cpp" "/Users/wenyongyan/Documents/文稿 - 闻永言的MacBook Pro/Documents/Code/C++/Reconstructor/cmake-build-release/CMakeFiles/Reconstructor.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/homebrew/include/eigen3"
+  "/opt/homebrew/Cellar/ceres-solver/2.0.0_4/include"
+  "/opt/homebrew/Cellar/glog/0.5.0/include"
+  "/opt/homebrew/Cellar/gflags/2.2.2/include"
+  "/opt/homebrew/Cellar/opencv/4.5.3_2/include/opencv4"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/Reconstructor.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reconstructor.dir/Camera.cpp.o"
+  "CMakeFiles/Reconstructor.dir/Correction.cpp.o"
+  "CMakeFiles/Reconstructor.dir/Image.cpp.o"
+  "CMakeFiles/Reconstructor.dir/Model.cpp.o"
+  "CMakeFiles/Reconstructor.dir/SfIS.cpp.o"
+  "CMakeFiles/Reconstructor.dir/iodata.cpp.o"
+  "CMakeFiles/Reconstructor.dir/main.cpp.o"
+  "Reconstructor"
+  "Reconstructor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reconstructor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
